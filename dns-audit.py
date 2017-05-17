35b40b68d1563ce11a5c0dfa75c5ad7a3b746ebe
mzarglis <mikefallensignup@gmail.com>
2017-05-17 15:06:50 -0400

    Added argparse, cleaned up

--- dns-audit.py
+++ dns-audit.py
@@ -1,12 +1,84 @@
-import dns.
+#!/usr/bin/python3
+
+import dns.resolver
 import os
+import argparse
+
+
+def parser():
+    parser = argparse.ArgumentParser(
+        description="simple dns_auditing script"
+    )
+    parser.add_argument(
+        "file",
+        type=str,
+        help="input file with list of IP's"
+
+    )
+    parser.add_argument(
+        "dns_server",
+        type=str,
+        help="IP adress of dns server"
+    )
+    return parser.parse_args()
+
+
+ARGS = parser()
+
+
+# Format Query for Reverse Lookup
+def format(ip):
+    req = '.'.join(reversed(ip.split("."))) + ".in-addr.arpa"
+    return req
+
+
+# Returns True if host responds to a ping request
+def ping(host):
+    import os, platform
+    # Ping parameters as function of OS
+    ping_str = "-n 1" if platform.system().lower() == "windows" else "-c 1"
+    # Ping
+    return os.system("ping " + ping_str + " " + host) == 0
+
+
+# Query Dns Server
+def query(request):
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = [ARGS.dns_server]
+
+    try:
+
+        answers = resolver.query(request, "PTR")
+        if len(answers) > 1:
+            global dnsDuplicates
+            dnsDuplicates += 1
+            print("Resolving  " + request)
+            with open(os.path.normpath("duplicates.txt"), 'a') as file:
+                file.write(request + "\n")
+            for rdata in answers:
+                with open(os.path.normpath("duplicates.txt"), 'a') as file:
+                    file.write(str(rdata) + '\n')
+                print(rdata)
+            with open(os.path.normpath("duplicates.txt"), 'a') as file:
+                file.write('\n')
+    except dns.resolver.NXDOMAIN:
+        print("No hostname present for " + request)
+    except Exception as e:
+        global dnsErrors
+        dnsErrors += 1
+        print("Resolving  " + request)
+        s = repr(e)
+        print("Query Failed with error:  " + s)
+        with open(os.path.normpath("errors.txt"), 'a') as file:
+            file.write("Query Failed for " + request + " with error:  " + s + "\n")
+
 
 dnsErrors = 0
 dnsDuplicates = 0
 
-def main():
 
-    file = open(os.path.normpath("C:/Users/mzarglis/Desktop/python/ip_list.txt"))
+def main():
+    file = open(os.path.normpath(ARGS.file))
     for line in file:
         line = line.strip()
         line = line.strip('\n')
@@ -17,47 +89,5 @@ def main():
     print("Total Dns Duplicates:  " + str(dnsDuplicates))
 
 
-#Format Query for Reverse Lookup
-def format(ip):
-    req = '.'.join(reversed(ip.split("."))) + ".in-addr.arpa"
-    return req
-#Returns True if host responds to a ping request
-def ping(host):
-  import os, platform
-  # Ping parameters as function of OS
-  ping_str = "-n 1" if  platform.system().lower()=="windows" else "-c 1"
-  # Ping
-  return os.system("ping " + ping_str + " " + host) == 0
-
-#Query Dns Server
-def query(request):
-    resolver = dns
-    resolver.nameservers = ['10.105.105.100']
-
-    try:
-
-        answers = resolver.query(request,"PTR")
-        if len(answers) > 1:
-            global dnsDuplicates
-            dnsDuplicates += 1
-            print("Resolving  " + request)
-            with open(os.path.normpath("C:/Users/mzarglis/Desktop/python/duplicates.txt"), 'a') as file:
-                file.write(request + "\n")
-            for rdata in answers:
-                with open(os.path.normpath("C:/Users/mzarglis/Desktop/python/duplicates.txt"), 'a') as file:
-                    file.write(str(rdata) + '\n')
-                print(rdata)
-            with open(os.path.normpath("C:/Users/mzarglis/Desktop/python/duplicates.txt"), 'a') as file:
-                 file.write( '\n')
-
-    except Exception as e:
-        global dnsErrors
-        dnsErrors += 1
-        print("Resolving  " + request)
-        s = repr(e)
-        print("Query Failed with error:  "+s)
-        with open(os.path.normpath("C:/Users/mzarglis/Desktop/python/errors.txt"), 'a') as file:
-            file.write("Query Failed for "+request+ " with error:  "+s + "\n")
-
-
-main()
+if __name__ == '__main__':
+    main()
